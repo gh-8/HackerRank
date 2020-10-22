e6c121551249becdffdc34ce795aebaf75257ef8
Ryan Serkouh <hserkouh@sfu.ca>
2020-10-21 21:10:44 -0700

    .c++ program to read numbers from stdin and print their sum to stdout

---
+++ c++/sum.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+Task
+Read  numbers from stdin and print their sum to stdout.
+Input Format
+One line that contains space-separated integers: a, b, and c.
+*/
+int main() {
+    int a=0,b=0,c=0;
+    cin >> a >> b >> c;
+    cout << (a+b+c) << endl;
+    return 0;
+}
